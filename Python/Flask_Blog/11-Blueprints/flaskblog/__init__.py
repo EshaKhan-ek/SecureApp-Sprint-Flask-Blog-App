00c6fccbd8c46abef7333b17c1293f9982c3c785
ha55454 <husnaanjum615@gmail.com>
2026-04-09 11:46:38 +0500

    Integrate CSRF protection into Flask application added
    Added CSRF protection to the Flask app and updated the app configuration to use a config class.

--- Python/Flask_Blog/11-Blueprints/flaskblog/__init__.py
+++ Python/Flask_Blog/11-Blueprints/flaskblog/__init__.py
@@ -3,26 +3,37 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
 from flaskblog.config import Config
 
-
+# ----------------- Extensions -----------------
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
-
+csrf = CSRFProtect()
+# ---------------------------------------------
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
+    # ---------------- Security Config ----------------
+    app.config['SECRET_KEY'] = 'supersecretkey'         # Required for Flask-WTF sessions
+    app.config['WTF_CSRF_ENABLED'] = True              # Enable CSRF protection
+    app.config['SESSION_COOKIE_SAMESITE'] = 'Strict'   # Protect cookies from cross-site requests
+    # --------------------------------------------------
+
+    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    csrf.init_app(app)
 
+    # Register blueprints
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
